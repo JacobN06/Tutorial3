1a36bc0decea56982a223f50c57e55f1d61c5559
JacobN06 <s34515@pjwstk.edu.pl>
2025-03-27 18:24:06 +0100

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,50 +1,111 @@
+import java.util.List;
+import java.util.UUID;
+
 public class Order {
-    private int orderID;
+    private String orderID;
+    private String customerId;
     private Customer customer;
-    private Product [] products;
-    private int [] quantitys;
+    private List<Product> products;
+    private List<Integer> quantitys;
     private String orderDate;
     private String status;
+    private boolean isLoyalCustomer;
+    int total = 0;
 
-    public Order(int orderID, Customer customer, Product [] products, String orderDate, String status) {
-        this.orderID = orderID;
+    public Order(String customerId, Customer customer, List<Product> products,List<Integer> quantitys, String orderDate,String status,boolean isLoyalCustomer) {
+        orderID = customerId + UUID.randomUUID().toString();
+        this.customerId = customerId;
         this.customer = customer;
         this.products = products;
+        this.quantitys = quantitys;
         this.orderDate = orderDate;
         this.status = status;
+        this.isLoyalCustomer = isLoyalCustomer;
     }
 
-    public int getOrderID() {
+    public String getOrderID() {
         return orderID;
     }
-    public void setOrderID(int orderID) {
+    public void setOrderID(String orderID) {
         this.orderID = orderID;
+    }
+    public String getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
     public Customer getCustomer() {
         return customer;
     }
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
+    public void setCustomer(Customer newCustomer) {
+        this.customer = newCustomer;
     }
-    public Product[] getProducts() {
+    public List<Product> getProducts() {
         return products;
     }
-    public void setProducts(Product[] products) {
-        this.products = products;
+    public void setProducts(List<Product> newProducts) {
+        this.products = newProducts;
     }
-    public int[] getQuantitys() {
+    public List<Integer> getQuantitys() {
         return quantitys;
     }
-    public void setQuantitys(int[] quantitys) {
-        this.quantitys = quantitys;
+    public void setQuantitys(List<Integer> newQuantitys) {
+        this.quantitys = newQuantitys;
     }
     public String getOrderDate() {
         return orderDate;
     }
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
+    public void setOrderDate(String newOrderDate) {
+        this.orderDate = newOrderDate;
     }
     public String getStatus() {
         return status;
+    }
+    public void setStatus(String newstatus) {
+        this.status = newstatus;
+    }
+    public boolean isLoyalCustomer() {
+        return isLoyalCustomer;
+    }
+    public void setLoyalCustomer(boolean loyalCustomer) {
+        isLoyalCustomer = loyalCustomer;
+    }
+
+    public int calculateTotalValue(){
+        int total = 0;
+        for (int i = 0; i < products.size(); i++) {
+            total += products.get(i).getPrice() * quantitys.get(i);
+        }
+        if(customer.getIsLoyalCustomer()){
+                int discountAmount = total % 10;
+                total -= discountAmount;
+        }
+            return total;
+
+    }
+    public int applyDiscount(){
+        if(customer.getIsLoyalCustomer()){
+            int discountAmount = calculateTotalValue() % 10;
+            total -= discountAmount;
+        }
+        return total;
+    }
+    public void displayOrderInfo(){
+        System.out.println("Order ID: " + orderID);
+        System.out.println("Customer: " + customer.getFirstName());
+        System.out.println("Products: ");
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println(products.get(i).getProductName());
+            System.out.print(" amount: " + quantitys.get(i));
+            System.out.println();
+        }
+
+        System.out.println("Total cost (with loyalty discount if available): " + calculateTotalValue());
+
+
+        System.out.println("Order Date: " + orderDate);
+        System.out.println("Status: " + status);
+        System.out.println();
     }
 }
